0236522a54638a7237a224fc8ee2b9915128884f
wulongtao <769418278@qq.com>
2019-01-29 18:00:36 +0800

    基础项目搭建

---
+++ src/main/java/com/tyilack/assist/ui/ScreenCaptureUi.java
@@ -0,0 +1,14 @@
+package com.tyilack.assist.ui;
+
+import com.xnx3.robot.ScreenCapture;
+
+/**
+ * @author wulongtao
+ */
+public class ScreenCaptureUi {
+
+    public static void main(String[] args) {
+        new ScreenCapture();
+    }
+
+}
